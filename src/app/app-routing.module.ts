2d8861302db7d618948c1da15a03ca94fffb6c2f
Sakthi Eswarasamy <sakthimaths123@gmail.com>
2020-12-24 03:41:17 +0530

    Application theme setup.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { UserComponent } from './modules/user/user.component';
+import { TaskComponent } from './modules/task/task.component';
+import { OrderComponent } from './modules/order/order.component';
+
+const routes: Routes = [
+  { path: 'home', component: AdminComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'task', component: TaskComponent },
+  { path: 'order', component: OrderComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContentComponent } from './layout/content/content.component';
+import { SideComponent } from './layout/side/side.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { UserComponent } from './modules/user/user.component';
+import { TaskComponent } from './modules/task/task.component';
+import { OrderComponent } from './modules/order/order.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    SideComponent,
+    AdminComponent,
+    UserComponent,
+    TaskComponent,
+    OrderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
